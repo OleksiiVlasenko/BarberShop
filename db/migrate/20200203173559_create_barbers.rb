6344fcc00d54895ba79e5ff879c98405051ce27a
FALLENRABBIT <a.a.a.vlasenko@gmail.com>
2020-02-03 20:13:33 +0200

    edit barbers migration initial values

--- db/migrate/20200203173559_create_barbers.rb
+++ db/migrate/20200203173559_create_barbers.rb
@@ -5,6 +5,10 @@ class CreateBarbers < ActiveRecord::Migration[6.0]
   		t.text :type_prof
   		t.text :time_work
   	end
-  	Barber.create :name=>'Alex',:type_prof=>'Парикмахер',:time_work=>'08:00 - 17:00'
+  	Barber.create :name=>'Наталія',:type_prof=>'Парикмахер',:time_work=>'08:00 - 17:00'
+  	Barber.create :name=>'Олена',:type_prof=>'Парикмахер',:time_work=>'08:00 - 17:00'
+  	Barber.create :name=>'Вікторія',:type_prof=>'Парикмахер',:time_work=>'10:00 - 19:00'
+  	Barber.create :name=>'Анастасія',:type_prof=>'Парикмахер',:time_work=>'09:00 - 18:00'
+  	Barber.create :name=>'Олена',:type_prof=>'Парикмахер',:time_work=>'10:00 - 17:00'
   end
 end
